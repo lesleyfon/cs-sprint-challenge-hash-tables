1f9e94f0d540783faed9b0142202b4531a867fdf
lesley <lesleybanadzem2017@gmail.com>
2020-08-16 14:34:41 -0700

    Completed exp1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,15 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    hash_map = {}
+    for i in range(length):
+        for k in range(length):
+            if i != k:
+                total = weights[i] + weights[k]
+                if total == limit:
+                    hash_map[total] = (i, k)
 
-    return None
+    if len(hash_map.keys()) == 0:
+        return None
+    else:
+        return hash_map[limit]
